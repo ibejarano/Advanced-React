9d5df8dfd033e755882b3824061eabb7b73bd406
Ignacio Bejarano <ignbejarano@gmail.com>
2019-12-02 22:28:04 -0300

    setting up Yoga

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    console.log(item);
 
-module.exports = mutations;
+    return item;
+  },
+};
+
+module.exports = Mutations;
