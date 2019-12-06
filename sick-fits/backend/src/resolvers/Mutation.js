65551d78f3ea838f64e8578de4ff58ef1a6f2728
Ignacio Bejarano <ignbejarano@gmail.com>
2019-12-06 18:42:36 -0300

    added the delete mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -12,6 +12,29 @@ const Mutations = {
 
     return item;
   },
+  updateItem(parent, args, ctx, info) {
+    // 1. take a copy of the updates
+    const updates = { ...args };
+    // 2. remove the id from updates
+    delete updates.id;
+    // 3 run the update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id,
+      },
+      info,
+    });
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1 find theitem
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // 2 check if they own that item or check permission
+    // TODO
+    // 3 delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
